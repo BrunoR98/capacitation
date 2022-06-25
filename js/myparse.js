785a9484b6720166101f9648655a83be0e6e4ed5
Bruno Renaudo <brunorenaudo@gmail.com>
2022-06-24 23:18:14 -0300

    Final release with styles
    Se acomodo un poco el código y se le dio estilos a la página.

--- js/myparse.js
+++ js/myparse.js
@@ -7,7 +7,7 @@
  */
 
 const textArea = document.getElementById('json-textarea');
-let convBtn = document.getElementById('conv-btn');
+const convBtn  = document.getElementById('conv-btn');
 
 let incomingJSON;
 
@@ -18,15 +18,17 @@ let obj1 = {};
 let obj2 = {};
 
 let singleLine = [];
-let firstMember = [];
-let secondsMember = [];
+
+let keys = [];
+let values = [];
 
 let prop = [];
 
 let subArray = [];
 let firstSubObj = [];
 let secondSubObj = [];
-let array = [];
+
+let finalArray = [];
 
 const validation = () => {
     if(textArea.value === ""){
@@ -46,11 +48,41 @@ const jsonParse = () => {
     }
 
     //Separo en dos arrays diferentes las claves y los valores
-    for(let i = 0; i < prop.length; i++){
-        firstMember.push(prop[i][0].replaceAll("\"","").replaceAll("\n","").replaceAll(" ","").replaceAll('{',''));
-        secondsMember.push(prop[i][1].replaceAll("\"","").replaceAll("\n","").replaceAll('}','').trim());
+    for(let i in prop){
+        keys.push(prop[i][0].replaceAll("\"","").replaceAll("\n","").replaceAll(" ","").replaceAll('{',''));
+        values.push(prop[i][1].replaceAll("\"","").replaceAll("\n","").replaceAll('}','').trim());
     }
 
+    internalArray();
+
+    //Enmascaro los valores dado que sino salen como string
+    firstSubObj[3]  = Number(firstSubObj[3]);
+    secondSubObj[3] = Number(secondSubObj[3]);
+    values[1]       = Number(values[1]);
+    values[6]       = Boolean(values[6]);
+
+    //Formo los dos objetos anidados dentro del array
+    finalArray.push(
+        obj1 = {
+                [firstSubObj[0]] : firstSubObj[1],
+                [firstSubObj[2]] : firstSubObj[3]
+                },
+        obj2 = {
+                [secondSubObj[0]] : secondSubObj[1],
+                [secondSubObj[2]] : secondSubObj[3]
+                });
+
+
+    json = {[keys[0]] : values[0],
+            [keys[1]] : values[1],
+            [keys[2]] : finalArray,
+            [keys[6]] : values[6],
+            [keys[7]] : values[7]}
+    
+    alert('Conversión exitosa, puede acceder al objeto json.');
+}
+
+const internalArray = () =>{
     /*Utilizo este array para trabajar el arreglo anidado,
     donde su primer elemento subArray[0] contiene ambos objetos
     */
@@ -58,49 +90,21 @@ const jsonParse = () => {
     subArray = subArray[1].split(']');
 
     singleLine = subArray[0].split(",");
+    
     prop = [];
-
     for(let single of singleLine){
         prop.push(single.split(" : "));
     }
 
-
-    for(let j = 0; j < prop.length; j++){
+    for(let j = 0; j < (prop.length / 2); j++){
         firstSubObj.push(prop[j][0].replaceAll("\"","").replaceAll("\n","").replaceAll(" ","").replaceAll('{',''))
         firstSubObj.push(prop[j][1].replaceAll("\"","").replaceAll("\n","").replaceAll('}','').trim());
     }
-
 
     for(let k = 2; k < prop.length; k++){
         secondSubObj.push(prop[k][0].replaceAll("\"","").replaceAll("\n","").replaceAll(" ","").replaceAll('{',''))
         secondSubObj.push(prop[k][1].replaceAll("\"","").replaceAll("\n","").replaceAll('}','').trim());
     }
-
-
-    //Enmascaro los valores dado que sino salen como string
-    firstSubObj[3] = Number(firstSubObj[3]);
-    secondSubObj[3] = Number(secondSubObj[3]);
-    secondsMember[1] = Number(secondsMember[1]);
-    secondsMember[6] = Boolean(secondsMember[6]);
-
-    //Formo los dos objetos anidados dentro del array
-
-    array.push(obj1 = {[firstSubObj[0]] : firstSubObj[1],
-            [firstSubObj[2]] : firstSubObj[3]
-    })
-
-    array.push(obj2 = {[secondSubObj[0]] : secondSubObj[1],
-        [secondSubObj[2]] : secondSubObj[3]
-    })
-
-    json = {[firstMember[0]] : secondsMember[0],
-        [firstMember[1]] : secondsMember[1],
-        [firstMember[2]] : array,
-        [firstMember[6]] : secondsMember[6],
-        [firstMember[7]] : secondsMember[7]}
-    
-    alert('Conversión exitosa, puede acceder al objeto json.');
-
 }
 
 convBtn.addEventListener('click', validation);
