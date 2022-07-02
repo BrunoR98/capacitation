bf59f7f03f650cdf03e608e01589dc7c48add22b
Bruno Renaudo <brunorenaudo@gmail.com>
2022-07-01 21:19:29 -0300

    Final comments

--- js/api.js
+++ js/api.js
@@ -1,3 +1,14 @@
+/**
+ * Author: Renaudo Bruno, Agustín.
+ * Date: 01/07/22
+ * Comments: You can use two functions, getUser return an array with ten user´s objects
+ * and getAlbums return an array of objects with user´s albums.
+ * - For getUsers, you just need to specify what 'forma' function you want to use.
+ * - For getAlbums, you need to add an user´s id.
+ * - 'forma' and 'id' are param´s.
+ *  Example: You can write in console => getUsers(forma1) or getAlbums(1,forma1)
+ */
+
 async function getUser(forma) {
     try{
         const output = await forma();
@@ -17,9 +28,9 @@ async function getAlbums(id, forma) {
 }
 
 /*
-En ambas funciones forma si son llamadas sin argumentos
-corresponde ejecutarse lo relacionado a users y con un argumento
-(id) corresponde ejecutarse lo relacionado a albums.
+    In both functions (forma1 and forma2), if they are called without arguments,
+    corresponds to execute what is related to users, and corresponds to execute what is
+    related to albums if they are called with id argument.
 */ 
 
 async function forma1() {
@@ -39,7 +50,7 @@ async function forma1() {
             const userAlbum = await albumPromise.json();
             return userAlbum;
         } catch (e) {
-            console.log(`Ha ocurrido un error inesperado: ${e}}`);
+            console.log(`Ha ocurrido un error inesperado: ${e}`);
         }
     }
 }
@@ -54,6 +65,7 @@ function forma2() {
                         .then(fetchUser => {return fetchUser.json()})
                         .then(userJson => {return userDestructuring(userJson)})
                         .then(userArray => {return userArray})
+                        .catch(e => {console.log(`Ha ocurrido un error inesperado: ${e}`)})
             );
             reject("Ha ocurrido un error inesperado");
         })
@@ -65,6 +77,7 @@ function forma2() {
                 albumPromise
                         .then(fetchAlbum => {return fetchAlbum.json()})
                         .then(userAlbum => {return userAlbum})
+                        .catch(e => {console.log(`Ha ocurrido un error inesperado: ${e}`)})
             );
             reject("Ha ocurrido un error inesperado");
         })
@@ -95,7 +108,7 @@ const userDestructuring = (users) => {
                 } 
         } = userObj;
 
-        //Creo el usuario respetando el destructuring sin las props indicadas.
+        //I create the user without the indicated props, respecting the destructuring.
         const newUser = {
                 id,
                 name,
