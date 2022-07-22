1df8c4930b280b614de45dc716d586087d8bd494
BrunoR98 <brunorenaudo@gmail.com>
2022-07-21 21:11:27 -0300

    First commit
    tp5 configurations y header listo

---
+++ trip-bnb/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Header />
+    </React.StrictMode>
+  );
+}
+
+export default App;
---
+++ trip-bnb/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    wrapper: {
+        borderBottom: '2px solid #bbb',
+        margin: 'auto',
+        width: '96%',
+    },
+    image: {
+        display: 'flex',
+        margin: '5px',
+        '& h2': {
+            marginLeft: '5px',
+        }
+    },
+    msg: {
+        position: 'absolute',
+        top: '40px',
+        right: '3%',
+        fontSize: '25px',
+        fontWeight: '400'
+    }
+});
+
+export default function Header() {
+    const classes = useStyles();
+    return(
+        <div className={classes.wrapper}>
+            <div className={classes.image}>
+                <img src='/images/tripbnb.jpg' alt='trip-bnb'/>
+                <h2>TripBnB</h2>
+            </div>
+            <div className={classes.msg}>
+                <span>Bienvenido Homero</span>
+            </div>
+        </div>
+    )
+}
