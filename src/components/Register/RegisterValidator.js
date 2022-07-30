2536b01f0f5e9acd0c57df759cd1c0f3def4a6c6
Bruno Renaudo <89168953+BrunoR98@users.noreply.github.com>
2022-07-30 19:45:36 -0300

    Delete src directory

--- src/components/Register/RegisterValidator.js
+++
@@ -1,9 +0,0 @@
-export default function RegisterValidator(user, data) {
-    for(let element of data){
-        if(element.email === user.email){
-            throw new Error('Email already exists.');
-        } else if (element.username === user.username) {
-            throw new Error('Username already exists.');
-        }
-    }
-}
